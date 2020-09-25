cc36a9d8fb7187b04eb67d3949b77ebcfc42bd28
Will Hughes <whukes@gmail.com>
2020-09-25 18:31:40 -0400

    Delete user.personal_notes_updated.js

--- src/handlers/user.personal_notes_updated.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-const logger = require('src/logger');
-
-module.exports = (payload) => {
-    logger.debug('handlers | ' + path.parse(__filename).name + ' | ' + payload.object.personal_notes);
-}
